3ae2cf09c58da989638749c34f3468d3c0ac1879
zahidmiran <zahidmiron599@gmail.com>
2022-08-02 16:52:36 +0600

    Navbar Compleate

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div>
-      <button className="inline-block cursor-pointer rounded-md bg-gray-800 px-4 py-3 text-center text-sm font-semibold uppercase text-white transition duration-200 ease-in-out hover:bg-gray-900">Button</button>
-
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+    <div className='ml-14 mr-14'>
+      <Navbar></Navbar>
+    
     </div>
   );
 }
